1e9c106ee8889fc1fbcc417740ae44d03d2ba377
Prabhat Kiran <mclaren.kiran@gmail.com>
2012-07-09 22:58:08 -0700

    WelcomeScreen done, Have to finish the Inventory Scene

---
+++ Contraptions/GenericLevelLayer.mm
@@ -0,0 +1,14 @@
+//
+//  GenericLevelLayer.m
+//  Contraptions
+//
+//  Created by Prabhat Kiran Bharathidhasan on 09/07/12.
+//  Copyright 2012 mclaren.kiran@gmail.com. All rights reserved.
+//
+
+#import "GenericLevelLayer.h"
+
+
+@implementation GenericLevelLayer
+
+@end
