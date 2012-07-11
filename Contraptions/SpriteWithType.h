bb1619cb324d055d7a0491702c88ea14acb2d741
Prabhat Kiran <mclaren.kiran@gmail.com>
2012-07-11 02:09:48 -0700

    Able to initialise Physics Objects. Have to implement contact Listener.

---
+++ Contraptions/SpriteWithType.h
@@ -0,0 +1,13 @@
+//
+//  SpriteWithType.h
+//  Contraptions
+//
+//  Created by Prabhat Kiran Bharathidhasan on 11/07/12.
+//  Copyright (c) 2012 mclaren.kiran@gmail.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SpriteWithType : NSObject
+
+@end
---
+++ Contraptions/SpriteWithType.m
@@ -0,0 +1,13 @@
+//
+//  SpriteWithType.m
+//  Contraptions
+//
+//  Created by Prabhat Kiran Bharathidhasan on 11/07/12.
+//  Copyright (c) 2012 mclaren.kiran@gmail.com. All rights reserved.
+//
+
+#import "SpriteWithType.h"
+
+@implementation SpriteWithType
+
+@end
