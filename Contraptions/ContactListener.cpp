bf4db902c99062da2af51f0cb29268915f650000
Prabhat Kiran <mclaren.kiran@gmail.com>
2012-07-11 05:07:44 -0700

    Have to get the Converyors working and set the Goal and the MSP is done.

---
+++ Contraptions/ContactListener.cpp
@@ -0,0 +1,9 @@
+//
+//  ContactListener.cpp
+//  Contraptions
+//
+//  Created by Prabhat Kiran Bharathidhasan on 11/07/12.
+//  Copyright (c) 2012 mclaren.kiran@gmail.com. All rights reserved.
+//
+
+#include <iostream>
---
+++ Contraptions/ContactListener.h
@@ -0,0 +1,14 @@
+//
+//  ContactListener.h
+//  Contraptions
+//
+//  Created by Prabhat Kiran Bharathidhasan on 11/07/12.
+//  Copyright (c) 2012 mclaren.kiran@gmail.com. All rights reserved.
+//
+
+#ifndef Contraptions_ContactListener_h
+#define Contraptions_ContactListener_h
+
+
+
+#endif
